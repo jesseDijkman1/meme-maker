29d39023d69796c13f85c4664b94cef4c0dd9199
Jesse Dijkman <jesse.dijkman@hva.nl>
2019-04-24 14:47:20 +0200

    aoiwejf

---
+++ app.js
@@ -0,0 +1,124 @@
+const express = require("express"),
+      session = require("express-session"),
+      ejs = require("ejs"),
+      multer  = require("multer"),
+      path = require("path"),
+      fs = require("fs");
+
+// ===== Multer ===== //
+const storage = multer.diskStorage({
+  destination: "./tempImgStorage",
+  filename: (req, file, callback) => callback(null, `${file.fieldname}-${req.session.id}${path.extname(file.originalname)}`)
+})
+
+const upload = multer({storage: storage})
+
+const port = 3000;
+
+let users = {},
+    memes = {}
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+app.use(express.static("public"));
+app.use(express.static('tempImgStorage'));
+app.use(session({secret: "Shh, its a secret!"}));
+
+class Meme {
+  constructor(data) {
+    this.userID = data.userID;
+    this.step = data.step;
+    this.imgName;
+  }
+
+  remove() {
+    fs.readdir("tempImgStorage", (err, files) => {
+      if (files.length) {
+        fs.unlink(`tempImgStorage/${this.imgName}`, (err) => {
+          if (err) throw err;
+        });
+      }
+    })
+  }
+}
+
+function cleanImgStorage(sessions) {
+  const allSessions = Object.keys(sessions);
+
+  if (Object.keys(memes).length) {
+    for (let meme in memes) {
+      if (!allSessions.includes(memes[meme].userID)) {
+        memes[meme].remove()
+      }
+    }
+  }
+
+  fs.readdir("tempImgStorage", (err, files) => {
+    files.forEach(file => {
+      const rx = /(?<=meme-).[^\.]+(?=.\w+)/;
+      const fileId = rx.exec(file)[0]
+
+      if (!allSessions.includes(fileId)) {
+        fs.unlink(`tempImgStorage/${file}`, (error) => {
+          if (error) throw error;
+        });
+      }
+    })
+  })
+
+
+}
+
+app.get("/", (req, res) => {
+  if (memes[req.session.id]) {
+    memes[req.session.id].remove()
+  }
+
+  cleanImgStorage(req.sessionStore.sessions)
+
+  res.render("index.ejs", {
+    sessionID: req.session.id
+  })
+});
+
+app.get("/meme-maker", (req, res) => {
+  const step = parseInt(req.query.s),
+        sessionID = req.query.id;
+
+  if (!sessionID || !step) return res.redirect("/");
+
+  switch (step) {
+    case 1:
+      memes[sessionID] = new Meme({
+        userID: sessionID,
+        step: step
+      })
+
+      res.render("imageUpload.ejs", {sessionID: sessionID})
+      break;
+
+    case 2:
+      res.render("textAdd.ejs", {
+        sessionID: sessionID,
+        imgName: memes[sessionID].imgName
+      })
+      break;
+  }
+})
+
+app.post('/uploadIMG', upload.single("meme"), (req, res) => {
+  const sessionID = req.body.sessionID,
+        fileName = req.file.filename;
+
+  memes[sessionID].imgName = fileName;
+
+  res.redirect(`/meme-maker?s=2&id=${sessionID}`)
+})
+
+app.post("/saveMeme", upload.single("meme"), (req, res) => {
+  console.log(req.file)
+})
+
+app.listen(port, () => console.log(`Listening to port: ${port}`));
---
+++ public/js/index.js
@@ -0,0 +1,295 @@
+"use strict";
+// (() => {
+const canvas = document.getElementById("meme-canvas");
+// const ctx = canvas.getContext("2d");
+
+const topTxtInput = document.getElementById("top-txt");
+const bottomTxtInput = document.getElementById("bottom-txt");
+
+const topTxtSizeInput = document.getElementById("top-txt-size");
+const bottomTxtSizeInput = document.getElementById("bottom-txt-size");
+
+const autoTxtPosInput = document.getElementById("auto-position");
+const topTxtPosInput = document.getElementById("top-txt-position");
+const bottomTxtPosInput = document.getElementById("bottom-txt-position");
+
+const colorSliders = document.querySelectorAll(".color-slider input[type=range]");
+const colorPreview = document.getElementById("color-preview");
+
+const downloadBtn = document.getElementById("download-meme");
+
+class Meme {
+  constructor(c) {
+    this.cWidth;
+    this.cHeight;
+    this.canvas = c;
+    this.ctx = c.getContext("2d");
+    this.img;
+    this.contentTop = "Hello \n World";
+    this.contentBottom = "";
+    this.topSize = 16;
+    this.bottomSize = 16;
+    this.color;
+    this.topPos;
+    this.bottomPos;
+    this.topTxt.bind(this)
+    this.bottomTxt.bind(this)
+    this.topTxtSize.bind(this)
+    this.bottomTxtSize.bind(this)
+
+    this.createImage().then(this.update.bind(this))
+  }
+
+  createImage() {
+    return new Promise((resolve, reject) => {
+      this.img = new Image()
+      this.img.src = this.canvas.dataset.img;
+
+      this.img.onload = e => {
+        this.cWidth = e.target.width;
+        this.cHeight = e.target.height;
+        this.topPos = this.cHeight / 20;
+        this.bottomPos = this.cHeight - this.cHeight / 20;
+
+        this.canvas.setAttribute("width", this.cWidth);
+        this.canvas.setAttribute("height", this.cHeight);
+
+        resolve()
+      }
+    })
+  }
+
+  update() {
+    this.ctx.clearRect(0, 0, this.cWidth, this.cHeight);
+
+    // Step 1: Draw the background
+    this.ctx.drawImage(this.img, 0, 0);
+
+    // Step 2: Set the color of the text
+    this.ctx.fillStyle = this.color || "white";
+
+    // Step 3: Set the size of the top text and draw the text
+    this.ctx.font = `${this.topSize}px Comic Sans MS`;
+    this.ctx.textAlign = "center";
+    this.ctx.textBaseline = "top";
+    this.ctx.fillText(this.contentTop, this.cWidth / 2, this.topPos);
+
+    // Step 4: Set the size of the bottom text and draw the text
+    this.ctx.font = `${this.bottomSize}px Comic Sans MS`;
+    this.ctx.textAlign = "center";
+    this.ctx.textBaseline = "alphabetic";
+    this.ctx.fillText(this.contentBottom, this.cWidth / 2, this.bottomPos);
+  }
+
+  topTxt(e) {
+    this.contentTop = e.target.value;
+    this.update()
+  }
+
+  bottomTxt(e) {
+    this.contentBottom = e.target.value;
+    this.update()
+  }
+
+  topTxtSize(e) {
+    this.topSize = parseInt(e.target.value);
+    this.update()
+  }
+
+  bottomTxtSize(e) {
+    this.bottomSize = parseInt(e.target.value);
+    this.update()
+  }
+
+  topTxtPos(e) {
+    this.topPos = parseInt(e.target.value)
+    this.update()
+  }
+
+  bottomTxtPos(e) {
+    this.bottomPos = this.cHeight - parseInt(e.target.value)
+    this.update()
+  }
+
+  autoTxtPos(e) {
+    if (e.target.checked) {
+      this.topPos = this.cHeight / 20;
+      this.bottomPos = this.cHeight - this.cHeight / 20;
+    }
+
+    this.update()
+  }
+
+  updateTxtColor(color) {
+    this.color = color;
+    this.update()
+  }
+}
+
+const meme = new Meme(canvas);
+
+const keys = {
+  37: -1,
+  38: 10,
+  39: 1,
+  40: -10,
+  188: -30,
+  190: 30
+}
+
+topTxtInput.addEventListener("keydown", e => {
+  setTimeout(() => meme.topTxt(e), 0)
+});
+
+bottomTxtInput.addEventListener("keydown", e => {
+  setTimeout(() => meme.bottomTxt(e), 0)
+});
+
+topTxtSizeInput.addEventListener("keydown", e => {
+
+  if (keys[e.keyCode]) {
+    e.preventDefault()
+
+    e.target.value = parseInt(e.target.value) + keys[e.keyCode]
+  }
+
+  setTimeout(() => meme.topTxtSize(e), 0)
+});
+
+bottomTxtSizeInput.addEventListener("keydown", e => {
+  if (keys[e.keyCode]) {
+    e.preventDefault()
+
+    e.target.value = parseInt(e.target.value) + keys[e.keyCode]
+  }
+
+  setTimeout(() => meme.bottomTxtSize(e), 0)
+});
+
+topTxtPosInput.addEventListener("keydown", e => {
+  if (keys[e.keyCode]) {
+    e.preventDefault()
+
+    e.target.value = parseInt(e.target.value) + keys[e.keyCode]
+  }
+
+  setTimeout(() => meme.topTxtPos(e), 0)
+})
+
+bottomTxtPosInput.addEventListener("keydown", e => {
+  if (keys[e.keyCode]) {
+    e.preventDefault()
+
+    e.target.value = parseInt(e.target.value) + keys[e.keyCode]
+  }
+
+  setTimeout(() => meme.bottomTxtPos(e), 0)
+})
+
+autoTxtPosInput.addEventListener("change", e => {
+  setTimeout(() => meme.autoTxtPos(e), 0)
+})
+
+void function() {
+  const colors = {}
+
+  function updateColor(colorType) {
+    const type = colorType;
+    const data = colors[colorType];
+
+    const color = `${type}(${Object.keys(data).map(key => {
+      return `${data[key].value}${data[key].unit}`
+    }).join(",")})`
+
+    colorPreview.style.setProperty("background", color)
+
+    meme.updateTxtColor(color);
+  }
+
+
+
+  for (let i = 0; i < colorSliders.length; i++) {
+    if (!colors[colorSliders[i].dataset.colorType]) {
+      colors[colorSliders[i].dataset.colorType] = {}
+    }
+
+    colors[colorSliders[i].dataset.colorType][colorSliders[i].dataset.char] = {
+      value: colorSliders[i].value,
+      unit: colorSliders[i].dataset.unit || ""
+    };
+    //
+    // const keys = {
+    //   37: -1,
+    //   38: 10,
+    //   39: 1,
+    //   40: -10,
+    //   188: -30,
+    //   190: 30
+    // }
+
+
+
+    let valDisplay;
+
+    if (colorSliders[i].nextElementSibling.classList.contains("slider-val")) {
+      valDisplay = colorSliders[i].nextElementSibling
+      valDisplay.max = colorSliders[i].max;
+      valDisplay.min = colorSliders[i].min;
+      valDisplay.value = colorSliders[i].value;
+    }
+
+    valDisplay.addEventListener("keydown", (e) => {
+      let val = parseInt(colorSliders[i].value);
+
+      if (keys[e.keyCode]) {
+        e.preventDefault()
+
+        val += keys[e.keyCode];
+      }
+
+      if (val < parseInt(colorSliders[i].max) && val > parseInt(colorSliders[i].min)) {
+        colorSliders[i].value = val;
+      } else {
+        if (val >= parseInt(colorSliders[i].max)) {
+          colorSliders[i].value = colorSliders[i].max
+        } else {
+          colorSliders[i].value = colorSliders[i].min
+        }
+      }
+
+      colorSliders[i].value = val;
+
+      colors[colorSliders[i].dataset.colorType][colorSliders[i].dataset.char].value = val;
+
+      updateColor(colorSliders[i].dataset.colorType)
+    })
+
+    colorSliders[i].addEventListener("keydown", (e) => {
+      let val = parseInt(colorSliders[i].value);
+
+      if (keys[e.keyCode]) {
+        e.preventDefault()
+
+        val += keys[e.keyCode];
+
+        colorSliders[i].value = val;
+
+        valDisplay.value = colorSliders[i].value;
+
+        colors[colorSliders[i].dataset.colorType][colorSliders[i].dataset.char].value = val;
+
+        updateColor(colorSliders[i].dataset.colorType)
+      }
+    })
+
+    colorSliders[i].addEventListener("change", (e) => {
+      valDisplay.value = colorSliders[i].value;
+
+      colors[colorSliders[i].dataset.colorType][colorSliders[i].dataset.char].value = colorSliders[i].value;
+
+      updateColor(colorSliders[i].dataset.colorType)
+    })
+  }
+
+  updateColor("rgb");
+}()
