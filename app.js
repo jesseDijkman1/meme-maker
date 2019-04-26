abb01bf8ea0bf5c580433738ba6d6cd81c8ad0e4
Jesse Dijkman <jesse.dijkman@hva.nl>
2019-04-26 13:32:30 +0200

    process.env.PORT for heroku

--- app.js
+++ app.js
@@ -1,21 +1,11 @@
 const express = require("express"),
       session = require("express-session"),
       ejs = require("ejs"),
-      // multer  = require("multer"),
       path = require("path"),
       fs = require("fs"),
       https = require('https');
 
-
-// // ===== Multer ===== //
-// const storage = multer.diskStorage({
-//   destination: "./tempImgStorage",
-//   filename: (req, file, callback) => callback(null, `${file.fieldname}-${req.session.id}${path.extname(file.originalname)}`)
-// })
-
-// const upload = multer({storage: storage})
-
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 let users = {},
     memes = {}
@@ -25,7 +15,6 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 app.use(express.static("public"));
-// app.use(express.static('tempImgStorage'));
 app.use(session({secret: "Shh, its a secret!"}));
 
 class Meme {
@@ -34,52 +23,9 @@ class Meme {
     this.step = data.step;
     this.imgName;
   }
-
-  // remove() {
-  //   fs.readdir("tempImgStorage", (err, files) => {
-  //     if (files.length) {
-  //       fs.unlink(`tempImgStorage/${this.imgName}`, (err) => {
-  //         if (err) throw err;
-  //       });
-  //     }
-  //   })
-  // }
 }
 
-// function cleanImgStorage(sessions) {
-//   const allSessions = Object.keys(sessions);
-//
-//   if (Object.keys(memes).length) {
-//     for (let meme in memes) {
-//       if (!allSessions.includes(memes[meme].userID)) {
-//         memes[meme].remove()
-//       }
-//     }
-//   }
-
-//   fs.readdir("tempImgStorage", (err, files) => {
-//     files.forEach(file => {
-//       const rx = /(?<=meme-).[^\.]+(?=.\w+)/;
-//       const fileId = rx.exec(file)[0]
-//
-//       if (!allSessions.includes(fileId)) {
-//         fs.unlink(`tempImgStorage/${file}`, (error) => {
-//           if (error) throw error;
-//         });
-//       }
-//     })
-//   })
-//
-//
-// }
-
 app.get("/", (req, res) => {
-  // if (memes[req.session.id]) {
-  //   memes[req.session.id].remove()
-  // }
-
-  // cleanImgStorage(req.sessionStore.sessions)
-
   res.render("index.ejs", {
     sessionID: req.session.id
   })
@@ -105,7 +51,6 @@ app.get("/meme-maker", async (req, res) => {
       break;
 
     case 2:
-    console.log(imgSrc)
       res.render("editor.ejs", {
         sessionID: sessionID,
         imgSrc: imgSrc
@@ -135,18 +80,5 @@ function getData(url) {
     });
   })
 }
-
-// app.post('/uploadIMG', upload.single("meme"), (req, res) => {
-//   const sessionID = req.body.sessionID,
-//         fileName = req.file.filename;
-//
-//   memes[sessionID].imgName = fileName;
-//
-//   res.redirect(`/meme-maker?s=2&id=${sessionID}`)
-// })
-
-// app.post("/saveMeme", upload.single("meme"), (req, res) => {
-//   console.log(req.file)
-// })
 
 app.listen(port, () => console.log(`Listening to port: ${port}`));
